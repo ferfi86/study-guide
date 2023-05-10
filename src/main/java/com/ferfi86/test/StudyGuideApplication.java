726cce18ec1db89230bf7c04cc7854a88b7cb4ba
luismejiac <luis.mejia@mercadolibre.com.mx>
2023-05-10 16:28:52 -0600

    first commit, creating project.

---
+++ src/main/java/com/ferfi86/test/StudyGuideApplication.java
@@ -0,0 +1,13 @@
+package com.ferfi86.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyGuideApplication.class, args);
+    }
+
+}
